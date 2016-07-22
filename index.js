ec35f65ca03b5db5a8938ba4775b6a91452b1fb8
Stefano Balietti <futur.dorko@gmail.com>
2016-07-22 16:09:17 -0400

    resolve

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ exports.require = function(file) {
     if (file === 'package.json') file = 'package.template.json';
     return require(path.resolve(__dirname, file));
 };
+
+// Return the path to the required template.
+exports.resolve = function(file) {
+    if (file === 'package.json') file = 'package.template.json';
+    return path.resolve(__dirname, file);
+};
