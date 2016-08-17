7a1b707d500b1bd34e56cdedd7ae456514835e78
Stefano Balietti <futur.dorko@gmail.com>
2016-08-17 16:56:27 -0400

    improved requirements.js

--- requirements/requirements.js
+++ requirements/requirements.js
@@ -3,7 +3,7 @@
  * Copyright(c) {YEAR} {AUTHOR} <{AUTHOR_EMAIL}>
  * MIT Licensed
  *
- * Sets requiremetns for accessing the channel.
+ * Processes the settings and sets the requirements for accessing the channel.
  *
  * http://nodegame.org
  * ---
@@ -12,8 +12,17 @@ module.exports = function(requirements, settings) {
 
     var ngr = require('nodegame-requirements');
 
-    requirements.add(ngr.nodegameBasic);
-    requirements.add(ngr.loadFrameTest);
+    var errBegin;
+    errBegin = 'An error occurred while loading the requirements for game ' +
+        requirements.gameName + '. Err: ';
+
+    if (settings.nodegameBasic !== false) {
+        requirements.add(ngr.nodegameBasic);
+    }
+
+    if (settings.loadFrameTest !== false) {
+        requirements.add(ngr.loadFrameTest);
+    }
 
     if (settings.cookieSupport) {
         requirements.add(ngr.cookieSupport, settings.cookieSupport);
@@ -22,17 +31,30 @@ module.exports = function(requirements, settings) {
     if ('object' === typeof settings.speedTest) {
         requirements.add(ngr.speedTest, settings.speedTest);
     }
+    else if ('undefined' !== typeof settings.speedTest) {
+        throw new TypeError(errBegin + 'speedTest must be object ' +
+                            'or undefined. Found: ' + settings.speedTest);
+    }
 
     if ('object' === typeof settings.viewportSize) {
         requirements.add(ngr.viewportSize, settings.viewportSize);
     }
-
-    if ('undefined' !== typeof settings.excludeBrowsers) {
-        requirements.add(ngr.browserDetect, settings.excludeBrowsers);
+    else if ('undefined' !== typeof settings.viewportSize) {
+        throw new TypeError(errBegin + 'viewportSize must be object ' +
+                            'or undefined. Found: ' + settings.viewportSize);
     }
 
-    if ('undefined' !== typeof settings.maxExecTime) {
+    if ('number' !== typeof settings.maxExecTime && settings.maxExecTime > 0) {
         requirements.setMaxExecutionTime(settings.maxExecTime);
+    }
+    else if ('number' !== typeof settings.maxExecTime) {
+        throw new TypeError(errBegin + 'maxExecTime must be number > 0 ' +
+                            'or undefined. Found: ' + settings.maxExecTime);
+    }
+
+    // Experimental.
+    if ('undefined' !== typeof settings.excludeBrowsers) {
+        requirements.add(ngr.browserDetect, settings.excludeBrowsers);
     }
 
     // requirements.add(ngr.testFail);
